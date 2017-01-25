bdfe4e27bc1398584383d4739f1462828de48a66
Razorcake <sp00kybug@yahoo.ca>
2017-01-25 15:11:53 -0800

    updated about me, new slider, new imgs, next fix homepage.

--- wp-content/themes/skillcrushstarter/page.php
+++ wp-content/themes/skillcrushstarter/page.php
@@ -9,17 +9,14 @@
 
 get_header(); ?>
 
-<section class="default-page">		
-	<div class="main-content">
+<section class="default-page">
+	<div class="page-content">
 		<?php while ( have_posts() ) : the_post(); ?>
-			<h2 class="entry-title"><?php the_title(); ?></h2>
 			<article class="post-entry">
 				<?php the_content(); ?>
 			</article>
 		<?php endwhile; ?>
 	</div>
-	
-	<?php get_sidebar(); ?>
 </section>
 
 <?php get_footer(); ?>
